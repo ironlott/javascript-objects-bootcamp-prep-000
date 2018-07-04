3242c2721b2dc2234dfedf59b10252f13adf85e5
thomasl.pittman@gmail.com <thomasl.pittman@gmail.com>
2018-07-04 22:50:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,5 +9,5 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  return playlist
+  return playlist.assign({}, playlist:artistName});
 }
