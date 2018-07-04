a2ba786cbd44290c00ac3273ef351f3c25913657
thomasl.pittman@gmail.com <thomasl.pittman@gmail.com>
2018-07-04 22:39:25 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var artistName;
+var songTitle;
+var playlist = {artistName, songTitle};
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle
+  return Object
+}
